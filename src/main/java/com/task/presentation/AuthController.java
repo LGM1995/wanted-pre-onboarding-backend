aaa950075b14717838ffc17d18ee6db22837a8d7
lgm1995 <rjsahr5045@gmail.com>
2023-08-12 11:33:28 +0900

    fix: add @Valid to AuthController

--- src/main/java/com/task/presentation/AuthController.java
+++ src/main/java/com/task/presentation/AuthController.java
@@ -9,21 +9,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/auth")
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<Token> login(@RequestBody Login login) {
+    public ResponseEntity<Token> login(@RequestBody @Valid Login login) {
         return ResponseEntity.ok(authService.login(login));
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<MemberResponse> signUp(@RequestBody MemberRequest memberRequest) {
+    public ResponseEntity<MemberResponse> signUp(@RequestBody @Valid MemberRequest memberRequest) {
         return ResponseEntity.ok(authService.singUp(memberRequest));
     }
 }
