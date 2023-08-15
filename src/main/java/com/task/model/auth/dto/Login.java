386c11de90f481d384c2856942570e1d9c3434d0
lgm1995 <rjsahr5045@gmail.com>
2023-08-15 16:10:05 +0900

    fix : Request Validation
    fix login and memberRequest validation feat.annotation

--- src/main/java/com/task/model/auth/dto/Login.java
+++ src/main/java/com/task/model/auth/dto/Login.java
@@ -1,22 +1,22 @@
 package com.task.model.auth.dto;
 
+import com.task.util.annotation.MailCheck;
+import com.task.util.annotation.PasswordCheck;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class Login {
 
-    @Email(message = "@는 필수 값 입니다.")
+    @MailCheck
     @NotBlank(message = "이메일은 필수 입력값 입니다.")
     private String email;
 
+    @PasswordCheck
     @NotBlank(message = "비밀번호는 필수 입력값 입니다.")
-    @Length(min = 8, message = "비밀번호는 8자 이상 입니다.")
     private String password;
 
     public Login(String email, String password) {
